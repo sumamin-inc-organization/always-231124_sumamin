d707c915d5c394c907d965d22d0418e25ecebebe
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-11-24 18:22:48 +0900

    html webpackプラグインをセットアップし、ページを追加する。 set up html webpack plug in and add the pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode:"development",
@@ -7,6 +8,16 @@ module.exports = {
         filename:"bundle.[contenthash].js",
         path:path.resolve(__dirname,"dist")
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Always',
+          filename:'index.html',
+          template:`./src/assets/html/index.html`,
+        }),
+        // new MiniCssExtractPlugin({
+        //   filename:"[name].[contentHash].css"
+        // })
+      ],
     module: {
         rules: [
           {
