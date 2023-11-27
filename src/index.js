7506795bfb96cd54c0c2c96e13b4cbe37552caea
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-11-27 16:53:39 +0900

    キービジュアルのスタイルを追加する

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./assets/css/common/reset.css"
-import "./assets/css/common/typography.css"
 import "./assets/css/common/navbar.css"
 import "./assets/css/common/utility.css"
 import "./assets/css/index/hero.css"
 import "./assets/css/index/main.css"
+import "./assets/css/common/typography.css"
