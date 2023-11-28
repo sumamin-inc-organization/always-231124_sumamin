37ed7d22cc82c09751d196ba798f1f9b35405af1
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-11-28 11:54:11 +0900

    ケースセクションのバナーを追加する

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import "./assets/css/common/utility.css";
 import "./assets/css/index/hero.css";
 import "./assets/css/index/main.css";
 import "./assets/css/index/about.css";
+import "./assets/css/index/case.css";
 import "./assets/css/common/typography.css";
