c89a8dd9a0e7dacb95153a8c26236961f16a8e9d
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-11-28 16:27:41 +0900

    ケースセクションにパララックス効果とデコレーション画像を追加する

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ import "./assets/css/index/main.css";
 import "./assets/css/index/about.css";
 import "./assets/css/index/case.css";
 import "./assets/css/common/typography.css";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let images = gsap.utils.toArray('.case-decor');
+
+images.forEach(image=> {
+    gsap.to(image,{
+        yPercent:-100 * image.dataset.speed,
+        ease:'none',
+        scrollTrigger:{
+            scrub:image.dataset.speed
+        }
+    })
+})
+
