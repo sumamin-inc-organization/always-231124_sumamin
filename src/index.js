86b0c7e373beb8038f3fb6649ac16e0773a565d6
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-01 16:09:57 +0900

    コンタクトセクションのフォームのためのHTMLを追加する

--- src/index.js
+++ src/index.js
@@ -31,46 +31,36 @@ images.forEach((image) => {
   });
 });
 
-
 //slider
 
+const slideContainer = document.querySelector(".track");
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".blog-card");
+const nextBtn = document.getElementById("next_btn");
+const prevBtn = document.getElementById("previous_btn");
 
-// startSlide();
+const firstClone = slides[0].cloneNode(true); //not working rn
+const secondClone = slides[1].cloneNode(true); //not working rn
 
-const slideContainer = document.querySelector('.track');
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.blog-card');
-const nextBtn = document.getElementById('next_btn');
-const prevBtn = document.getElementById('previous_btn');
-
-const firstClone = slides[0].cloneNode(true);
-const secondClone = slides[1].cloneNode(true);
-
-
-slider.append(firstClone);
-
-
+slider.append(firstClone); //not working rn
 
 let index = 0;
 const slideWidth = slides[index].clientWidth;
 
-nextBtn.addEventListener('click', () => {
-  index++;
-  console.log(index)
+nextBtn.addEventListener("click", () => {
+  index++; //increases the index by 1
   if (index === 5) {
-    index = 0;
+    index = 0; //if its in the 5th slide it will reset to zero so that the first slide is shown
   }
   updateSlidePosition();
 });
 
-
-prevBtn.addEventListener('click', () => {
-  if(index === 0){
-    index = 4;
+prevBtn.addEventListener("click", () => {
+  if (index === 0) {
+    index = 4; //if its currently in the first slide it will jump to the last
     updateSlidePosition();
-  }else{
+  } else {
     index--;
-    console.log(index)
     if (index < 0) {
       index = slides.length - 1;
     }
@@ -79,7 +69,7 @@ prevBtn.addEventListener('click', () => {
 });
 
 function updateSlidePosition() {
-  const offset = 440 * index;
+  const offset = 440 * index; //off set is calculated with the card size and the gap (400px + 40)
   setIndicator();
   slides.forEach((slide, i) => {
     slide.style.transform = `translateX(${offset}px)`;
@@ -88,25 +78,22 @@ function updateSlidePosition() {
 
 //slider indicator
 
-const indicators = document.querySelectorAll('.indicator');
+const indicators = document.querySelectorAll(".indicator");
 
-const setIndicator = ()=>{
-  indicators.forEach((indicator)=>{
-    indicator.classList.remove('active');
-    const indicatorNumber = +(indicator.dataset.indicator);
-    if (indicatorNumber === index){
-      indicator.classList.add('active');
+const setIndicator = () => {
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("active"); //remove the active class
+    const indicatorNumber = +indicator.dataset.indicator;
+    if (indicatorNumber === index) {
+      indicator.classList.add("active"); //add active class to the clicked indicator
     }
-    
-  })
-}
+  });
+};
 
-
-indicators.forEach((indicator)=>{
-  indicator.addEventListener('click',(e)=>{
+indicators.forEach((indicator) => {
+  indicator.addEventListener("click", (e) => {
     const clickedIndicator = e.target;
-    index = parseInt(clickedIndicator.dataset.indicator);
+    index = parseInt(clickedIndicator.dataset.indicator); //make the index the same as the coreesponding indicator
     updateSlidePosition();
-
-  })
-})
+  });
+});
