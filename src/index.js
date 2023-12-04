10ed8cea10b5c9e0eb6f896b97f30390f5d40d6f
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-04 17:49:33 +0900

    comment out barbar js to prep for the about section

--- src/index.js
+++ src/index.js
@@ -40,6 +40,19 @@ images.forEach((image) => {
   });
 });
 
+// function contentAnimation(){
+//   let images = gsap.utils.toArray(".case-decor");
+
+//   images.forEach((image) => {
+//     gsap.to(image, {
+//       yPercent: -100 * image.dataset.speed,
+//       ease: "none",
+//       scrollTrigger: {
+//         scrub: image.dataset.speed,
+//       },
+//     });
+//   });
+// }
 /*----------------------------
  SLIDER
  スライダー
@@ -88,6 +101,71 @@ function updateSlidePosition() {
   });
 }
 
+// function slider(){
+
+// const slideContainer = document.querySelector(".track");
+// const slider = document.querySelector(".slider");
+// const slides = document.querySelectorAll(".blog-card");
+// const nextBtn = document.getElementById("next_btn");
+// const prevBtn = document.getElementById("previous_btn");
+
+// const firstClone = slides[0].cloneNode(true); //not working rn
+// const secondClone = slides[1].cloneNode(true); //not working rn
+
+// slider.append(firstClone); //not working rn
+
+// let index = 0;
+// const slideWidth = slides[index].clientWidth;
+
+// nextBtn.addEventListener("click", () => {
+//   index++; //increases the index by 1
+//   if (index === 5) {
+//     index = 0; //if its in the 5th slide it will reset to zero so that the first slide is shown
+//   }
+//   updateSlidePosition();
+// });
+
+// prevBtn.addEventListener("click", () => {
+//   if (index === 0) {
+//     index = 4; //if its currently in the first slide it will jump to the last
+//     updateSlidePosition();
+//   } else {
+//     index--;
+//     if (index < 0) {
+//       index = slides.length - 1;
+//     }
+//     updateSlidePosition();
+//   }
+// });
+
+// function updateSlidePosition() {
+//   const offset = 440 * index; //off set is calculated with the card size and the gap (400px + 40)
+//   setIndicator();
+//   slides.forEach((slide, i) => {
+//     slide.style.transform = `translateX(${offset}px)`;
+//   });
+// }
+// const indicators = document.querySelectorAll(".indicator");
+
+// const setIndicator = () => {
+//   indicators.forEach((indicator) => {
+//     indicator.classList.remove("active"); //remove the active class
+//     const indicatorNumber = +indicator.dataset.indicator;
+//     if (indicatorNumber === index) {
+//       indicator.classList.add("active"); //add active class to the clicked indicator
+//     }
+//   });
+// };
+
+// indicators.forEach((indicator) => {
+//   indicator.addEventListener("click", (e) => {
+//     const clickedIndicator = e.target;
+//     index = parseInt(clickedIndicator.dataset.indicator); //make the index the same as the coreesponding indicator
+//     updateSlidePosition();
+//   });
+// });
+// }
+
 /*----------------------------
   SLIDER INDICATOR
   スライダーのインジケーター
@@ -124,38 +202,44 @@ activateSmoothScroll();
 //transitopn 
 
 
-function delay(n) {
-  n = n || 2000
+// function delay(n) {
+//   n = n || 2000
   
-  return new Promise(resolve => {
-    setTimeout(resolve, n)
-  })
-}
+//   return new Promise(resolve => {
+//     setTimeout(resolve, n)
+//   })
+// }
 
 
-const loadingScreen = document.querySelector('.transition');
-function pageTransitionIn(){
-  let tl = gsap.timeline();
-    tl.to(loadingScreen, { duration: 0.7, x: 0, transformOrigin: 'bottom left'})
-   .to(loadingScreen, { duration: 0.7, x:"-100%", transformOrigin: 'bottom left', delay: 0.3})
-   .set(loadingScreen, {  x:  "100%"})
+// const loadingScreen = document.querySelector('.transition');
+// function pageTransitionIn(){
+//   let tl = gsap.timeline();
+//     tl.to(loadingScreen, { duration: 0.7, x: 0, transformOrigin: 'bottom left'})
+//    .to(loadingScreen, { duration: 0.7, x:"-100%", transformOrigin: 'bottom left', delay: 0.3})
+//    .set(loadingScreen, {  x:  "100%"})
       
-}
+// }
 
-barba.init({
-  sync:true,
-  transitions:[
-      {
-          async leave(data){
-              const done = this.async();
-              pageTransitionIn();
+// barba.init({
+//   sync:true,
+//   transitions:[
+//       {
+//           async leave(data){
+//               const done = this.async();
+//               pageTransitionIn();
               
-              await delay(1000);
-              done();
-          },
-          async enter(data){
-            
-          }
-      }
-  ]
-})
+//               await delay(1000);
+//               done();
+//           },
+//           async enter(data){
+//             contentAnimation()
+//             slider();
+//           },
+
+//           async once(data) {
+//             contentAnimation();
+//             slider();
+//         },
+//       }
+//   ]
+// })
