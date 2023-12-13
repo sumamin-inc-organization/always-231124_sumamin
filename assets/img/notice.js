557c202af3f4672873b3e423e65015b7049c812e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-13 14:12:46 +0900

    Updates

---
+++ assets/img/notice.js
@@ -0,0 +1,23 @@
+// import "./assets/css/common/reset.css";
+// import "./assets/css/common/navbar.css";
+// import "./assets/css/common/footer.css";
+// import "./assets/css/common/typography.css";
+// import "./assets/css/common/utility.css";
+// import "./assets/css/common/animation.css";
+// import "./assets/css/common/under.css";
+// import "./assets/css/under/notice.css";
+
+var moreNum = 10;
+$('.news_items:nth-child(n + ' + (moreNum + 1) + ')').addClass('is-hidden');
+$('.section_btn').on('click', function() {
+    $('.news_items.is-hidden').slice(0, moreNum).removeClass('is-hidden');
+    if ($('.news_items.is-hidden').length == 0) {
+        $('.section_btn').fadeOut();
+    } 
+});
+$(function() {
+    var list = $(".news_content .news_items").length;  
+    if (list < moreNum) {
+        $('.section_btn').addClass('is-btn-hidden');
+    }
+});
