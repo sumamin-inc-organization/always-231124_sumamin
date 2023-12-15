98fccecfa613ea887a4fdcb52372c7a28e14de5e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-15 16:27:00 +0900

    Aboutページのカルーセルを完成させる

--- src/about.js
+++ src/about.js
@@ -24,3 +24,67 @@ animateHamburgerMenu();
 ----------------------------*/
 
 toggleNav();
+
+
+/*----------------------------
+ ABOUT CAROUSEL 
+ Aboutカルーセル
+----------------------------*/
+
+const caseSlideContainer= document.querySelector('.case-card-carousel'); //スライド / カルーセルのコンテナ
+const caseSlide = document.querySelector('.about-card-carousel_list'); //カルーセルのトラック
+let caseSlides = document.querySelectorAll('.about-card-mobile');   //スライド / カード
+
+// 「次へ」ボタンと「前へ」ボタン
+const caseNextBtn = document.getElementById('about-next');
+const casePrevBtn = document.getElementById('about-prev');
+
+let sliderIndex = 0;
+console.log(caseNextBtn)
+
+// console.log(slideWidth)
+
+const nextSlide = () => {
+  caseSlides = document.querySelectorAll('.about-card-mobile');
+  if (sliderIndex >= caseSlides.length - 1) {
+    return;
+  } else {
+    sliderIndex++;
+    caseSlides.forEach((slide) => {
+      const slidesWidth = slide.clientWidth; // Use slide.clientWidth directly
+      const gap = 60;
+      const offset = slidesWidth + gap ;
+      slide.style.transform = `translateX(${-offset * sliderIndex}px)`;
+      slide.style.transition = '0.7s';
+    });
+  }
+};
+
+const prevSlide = () => {
+  caseSlides = document.querySelectorAll('.about-card-mobile');
+  if (sliderIndex === 0) {
+    return;
+  } else {
+    sliderIndex--;
+    caseSlides.forEach((slide) => {
+      const slidesWidth = slide.clientWidth; // Use slide.clientWidth directly
+      const gap = 60;
+      const offset = slidesWidth + gap ;
+      slide.style.transform = `translateX(${-offset * sliderIndex}px)`;
+      slide.style.transition = '0.7s';
+    });
+  }
+};
+
+
+// slide.style.transition ='0.7s';
+caseNextBtn.addEventListener('click',(e)=>{
+nextSlide()
+})
+
+console.log(sliderIndex);
+
+casePrevBtn.addEventListener('click',(e)=>{
+ prevSlide();
+})
+
